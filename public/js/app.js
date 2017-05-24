d8f9a8c66410bbf556cbfd49ac2d5b2f83f0e408
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-24 19:58:19 -0400

    First AJAX request works

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,20 @@
 console.log("Teacher Connection app.js works");
 
+var appURL = 'http://localhost:3000/';
+
 var app = angular.module('teachersConnection', []);
 
 app.controller('mainController', ['$http', function($http){
     this.message = "Controller works";
+
+    $http({
+      method: 'GET',
+      url: appURL+'teachers'
+    }).then(function(response){
+      console.log(response.data);
+    });
+
+
 
 
 

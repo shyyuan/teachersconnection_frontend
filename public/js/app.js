8ab2cfe5eccb2fbb3683c6f8979d683387e4b7d6
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-25 14:45:28 -0400

    Updated AJAX reqesut using arrow function

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,21 @@
 console.log("Teacher Connection app.js works");
 
-//var appURL = 'http://localhost:3000/';
-var appURL = 'https://teachersconnection-api.herokuapp.com/';
+const appURL = 'http://localhost:3000/';
+//var appURL = 'https://teachersconnection-api.herokuapp.com/';
 
-var app = angular.module('teachersConnection', []);
+const app = angular.module('teachersConnection', []);
+
+const loggedIn = false;
+const showInvalidLogin = false;
+const loginEmail = ''
+
 
 app.controller('mainController', ['$http', function($http){
   //this.message = "Controller works";
   // boolean variables check login
-  this.loggedIn = false;
-  this.showInvalidLogin = false;
-  this.loginEmail = '';
+  //this.loggedIn = true;
+  // this.showInvalidLogin = false;
+  //this.loginEmail = 'shyyuan@yahoo.com';
 
   // Arrays and Objects for the site
   this.activeTeacher = {};
@@ -22,14 +27,7 @@ app.controller('mainController', ['$http', function($http){
   // ============
   this.validateEmail = function(){
     //console.log(this.loginEmail);
-    $http({
-      method: 'GET',
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      url: appURL+'teachers'
-    }).then(function(response){
-      //console.log('Get teachers from server: ', response.data);
+    $http.get(appURL+'teachers').then(response => {
       for (var i=0; i<response.data.length; i++) {
         //console.log(response.data[i].email);
         if (response.data[i].email == this.loginEmail) {
@@ -45,25 +43,66 @@ app.controller('mainController', ['$http', function($http){
       }
       if (this.loggedIn){
         this.allTeachers = response.data;
-        //console.log(this.allTeachers);
+        console.log(this.allTeachers);
         this.getAllEvents();
       }
-    }.bind(this));
-  };
+    });
+
+    // $http({
+    //   method: 'GET',
+    //   headers: {
+    //     "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    //   },
+    //   url: appURL+'teachers'
+    // }).then(function(response){
+    //   //console.log('Get teachers from server: ', response.data);
+    //   for (var i=0; i<response.data.length; i++) {
+    //     //console.log(response.data[i].email);
+    //     if (response.data[i].email == this.loginEmail) {
+    //       this.loggedIn = true;
+    //       this.showInvalidLogin = false;
+    //       this.activeTeacher = response.data[i];
+    //       //console.log('Valid email: ', this.activeTeacher);
+    //       break;
+    //     } else {
+    //       this.loggedIn = false;
+    //       this.showInvalidLogin = true;
+    //     }
+    //   }
+    //   if (this.loggedIn){
+    //     this.allTeachers = response.data;
+    //     //console.log(this.allTeachers);
+    //     this.getAllEvents();
+    //
+    //   }
+    // }.bind(this));
+  };  // end of this.validateEmail function
 
   // get allEvents
   this.getAllEvents = function(){
-    $http({
-      method: 'GET',
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      url: appURL+'events'
-    }).then(function(response){
-      console.log('All events: ', response.data);
+    $http.get(appURL+'events').then(response => {
       this.allEvents = response.data;
-    }.bind(this));
+      console.log('All events: ', this.allEvents);
+    });
+    // $http({
+    //   method: 'GET',
+    //   headers: {
+    //     "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    //   },
+    //   url: appURL+'events'
+    // }).then(function(response){
+    //   console.log('All events from server: ', response.data);
+    //   this.allEvents = response.data;
+    //   console.log('All events: ', this.allEvents);
+    // }.bind(this));
+  };
+
+
+  this.logout = function(){
+    this.loggedIn = false;
+    this.showInvalidLogin = false;
+    this.loginEmail = ''
   }
 
-
+  //this.validateEmail();
 }]);
